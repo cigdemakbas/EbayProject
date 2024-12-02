bdfe0187164cef541d5ad4a5b5c9850792ade98d
Cigdem Akbas <cigdemtazegul@hotmail.com>
2024-12-02 11:06:59 -0500

    my test

--- src/test/java/UI/Ebay/StepDefinitions/CtegoriesFunctionSteps.java
+++ src/test/java/UI/Ebay/StepDefinitions/CtegoriesFunctionSteps.java
@@ -17,26 +17,28 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class CtegoriesFunctionSteps extends SearchBoxElements {
-    SearchBoxElements sbe= new SearchBoxElements();
+    SearchBoxElements sbe = new SearchBoxElements();
+
     @When("Click the categories section")
     public void clickTheCategoriesSection() {
-        WebElement categories= Driver.getDriver().findElement(By.id("gh-cat"));
+        WebElement categories = Driver.getDriver().findElement(By.id("gh-cat"));
         categories.click();
     }
+
     @And("User should click and navigate every category")
     public void userShouldClickAndNavigateEveryCategory(DataTable dt) throws InterruptedException {
         WebElement categories = Driver.getDriver().findElement(By.id("gh-cat"));
         Select list = new Select(categories);
         List<WebElement> categoriesList = list.getOptions();
         List<String> myList = dt.asList();
-        System.out.println("This comes from table : "+myList);
-        System.out.println("This comes from select class"+categoriesList);
+        System.out.println("This comes from table : " + myList);
+        System.out.println("This comes from select class" + categoriesList);
 
         for (int i = 0; i < categoriesList.size(); i++) {
-//            String dropdownText = categoriesList.get(i).getText();
-//            list.selectByVisibleText(dropdownText);
-//            String expectedText = myList.get(i);
-            Assert.assertEquals(expectedText,dropdownText);
+            String dropdownText = categoriesList.get(i).getText();
+            list.selectByVisibleText(dropdownText);
+            String expectedText = myList.get(i);
+            Assert.assertEquals(expectedText, dropdownText);
         }
 
     }
